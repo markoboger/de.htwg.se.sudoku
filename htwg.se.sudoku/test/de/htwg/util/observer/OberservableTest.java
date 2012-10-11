052ac0e34ea91f21cf7a2a1decc40930410afdce
markoboger <marko.boger@gmail.com>
2012-10-11 23:55:54 +0200

    added tests for the observer package. 100% coverage for this package now.

---
+++ htwg.se.sudoku/test/de/htwg/util/observer/OberservableTest.java
@@ -0,0 +1,51 @@
+package de.htwg.util.observer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OberservableTest {
+	private boolean ping=false;
+	private TestObserver testObserver;
+	private Observable testObservable;
+	
+	class TestObserver implements IObserver {
+		@Override
+		public void update() {
+			ping=true;
+		}
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		testObserver = new TestObserver();
+		testObservable = new Observable();
+		testObservable.addObserver(testObserver);
+	}
+
+	@Test
+	public void testNotify() {
+		assertFalse(ping);
+		testObservable.notifyObservers();
+		assertTrue(ping);
+	}
+	
+	@Test
+	public void testRemove() {
+		assertFalse(ping);
+		testObservable.removeObserver(testObserver);
+		testObservable.notifyObservers();
+		assertFalse(ping);
+	}
+	
+	@Test
+	public void testRemoveAll() {
+		assertFalse(ping);
+		testObservable.removeAllObservers();
+		testObservable.notifyObservers();
+		assertFalse(ping);
+	}
+
+}
