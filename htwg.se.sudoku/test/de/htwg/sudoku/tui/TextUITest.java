b5b319da18a582a9549d2d007ab09fb0ff1b0df0
mboger <marko.boger@htwg-konstanz.de>
2012-11-16 12:41:14 +0100

    broke up tui tests into single tests

--- htwg.se.sudoku/test/de/htwg/sudoku/tui/TextUITest.java
+++ htwg.se.sudoku/test/de/htwg/sudoku/tui/TextUITest.java
@@ -30,26 +30,62 @@ public class TextUITest {
 	}
 	
 	@Test
-	public void testStatus() {
+	public void testStatus001() {
 		assertEquals("Welcome to HTWG Sudoku!",controller1.getStatus());
 		tui1.processInputLine("001");
 		assertEquals("The cell (0,0) = 1 was successfully set",controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusr() {
 		tui1.processInputLine("r");
 		assertEquals("Sudoku was reset",controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusn() {
 		tui1.processInputLine("n");
 		assertEquals("New Sudoku Puzzle created",controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusc() {
 		tui1.processInputLine("c");
 		assertEquals("Copied Sudoku", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusp() {
 		tui1.processInputLine("p");
 		assertEquals("Pasted Sudoku", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusz() {
 		tui1.processInputLine("z");
 		assertEquals("Undo", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatusy() {
 		tui1.processInputLine("y");
 		assertEquals("Redo", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatus1() {
 		tui1.processInputLine("1");
 		assertEquals("Sudoku was reset", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatus4() {
 		tui1.processInputLine("4");
 		assertEquals("Sudoku was reset", controller1.getStatus());
+}
+	
+	@Test
+	public void testStatus9() {
 		tui1.processInputLine("9");
 		assertEquals("Sudoku was reset", controller1.getStatus());
 	}
